7c22706dfc481a30909cca7a0efb1f365f3da4f6
prasadcl <prasadclwork@gmail.com>
2020-07-18 23:22:04 +0530

    updating files for github pages

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ class App extends Component {
     const { monsters, searchField} = this.state;
     const filteredMonsters = monsters.filter(monster => 
         monster.name.toLowerCase().includes(searchField.toLowerCase())
-      )
+      );
 
     return (
       <div className="App">
