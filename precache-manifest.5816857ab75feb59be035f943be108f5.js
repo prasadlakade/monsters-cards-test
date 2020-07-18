9f499b107c27146a51aea0cc5d719388258923f6
prasadcl <prasadclwork@gmail.com>
2020-07-18 22:53:49 +0530

    Updates

---
+++ precache-manifest.5816857ab75feb59be035f943be108f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfbf7ee55c6076b3be2e1b9d67bcf2ff",
+    "url": "/monsters-cards-test/index.html"
+  },
+  {
+    "revision": "7392ea5156eac9f552f5",
+    "url": "/monsters-cards-test/static/css/main.79100a7d.chunk.css"
+  },
+  {
+    "revision": "e7187a0299b1635d14b4",
+    "url": "/monsters-cards-test/static/js/2.d0bb573d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-cards-test/static/js/2.d0bb573d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7392ea5156eac9f552f5",
+    "url": "/monsters-cards-test/static/js/main.77f1672f.chunk.js"
+  },
+  {
+    "revision": "98c3d95cd039fa4c4093",
+    "url": "/monsters-cards-test/static/js/runtime-main.bc802f41.js"
+  }
+]);
